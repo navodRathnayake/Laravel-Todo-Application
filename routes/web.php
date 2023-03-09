bf90a9e1c9abf0f03e4f19a68dd9be120196384e
Navod Rathnayake <82445185+navodRathnayake@users.noreply.github.com>
2023-03-09 12:25:48 +0530

    added home controllers and relevent routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index'])->name('home');
+
+Route::get('/todo',[TodoController::class,'index'])->name('todo');
